a1b9f2a137f4db937af2842b44209b2c01a2eccb
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-20 15:31:28 +0530

    Create line.py

---
+++ line.py
@@ -0,0 +1,9 @@
+a=int(input("enter the string)):
+count=0
+word=0
+for i in range:
+count=word+1:
+print("the count of character in the string")
+print("count")
+print("the word of the character in the string")
+print("word")
