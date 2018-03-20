86108572586cd5637b24fa77353037ed1327925d
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-20 16:12:08 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,7 @@
+a=int(input("enter the numbers"):
+for i in range(1,n+1):
+    b=int(input("Enter element:"))
+    a.append(b)
+a.sort()
+print("Largest element is:",a[n-1])
+print("display the maxinum numbers in a list")
