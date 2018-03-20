5e12889a7199bfa74f3cf0621e3ce5516a2ba78a
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-20 16:34:10 +0530

    Create index2.py

---
+++ index2.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number ")):
+    while (1)
+        while (i < n) && ((i+arr[i]) % 2 == 0)
+           i++
+        while (0 <= j) && ((j+arr[j]) % 2 == 0)    
+           j++
+    if j <= i    
+        break
+    swap(arr[i], arr[j])
+    print(" display the sorted number in index")
