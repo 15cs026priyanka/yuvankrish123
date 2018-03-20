95e8dd916b98b1b75fe9924b5a33c4b977f26b19
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-20 16:29:12 +0530

    Create largest1.py

---
+++ largest1.py
@@ -0,0 +1,7 @@
+a=int(input("enter the numbers")):
+    max = list[ 0 ]
+    for a in list:
+        if a > max:
+            max = a
+    return max:
+    print("display the largest numbers in the list")
