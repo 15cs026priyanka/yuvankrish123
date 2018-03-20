5368d811d49a36b8a7e919dea755c158d2e9cd3d
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-20 15:52:09 +0530

    Create character.py

---
+++ character.py
@@ -0,0 +1,9 @@
+a=int(input("enter a string")):
+char=1
+for a in range of character
+print("count the numeric character")
+print("char")
+else:
+print("this not an numeric character")
+print("char")
+print("print the numeric character")
